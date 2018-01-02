0f845cf00a659d11fd3942630b86dd27f66b21b9
mypiandrew <andrew@embeddedpi.com>
2018-01-02 15:49:50 +0000

    Update createdbs.sh

--- createdbs.sh
+++ createdbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Creating New DBs"
-sqlite3 db_data.db <db_data.sql
-sqlite3 db_config.db <db_config.sql
+sqlite3 -echo db_data.db <db_data.sql
+sqlite3 -echo db_config.db <db_config.sql
 
