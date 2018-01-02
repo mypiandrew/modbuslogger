1bf39420ec48bc75b5d86638e62d22aeaf2f1415
mypiandrew <andrew@embeddedpi.com>
2018-01-02 16:28:35 +0000

    Update db_config.sql
    Old file format used originally

--- db_config.sql
+++ db_config.sql
@@ -1,5 +1,5 @@
 /*
-	Database	: db_config.db
+        Database        : db_config.db
 
         The MIT License (MIT)
 
@@ -26,28 +26,32 @@
 
 */
 
-PRAGMA foreign_keys = OFF;
-
--- ----------------------------
--- Table structure for "main"."config"
--- ----------------------------
-DROP TABLE "config";
-CREATE TABLE "config" (
-"logInterval"  INTEGER NOT NULL DEFAULT 5,
-"transferInterval"  INTEGER NOT NULL DEFAULT 30,
-"dsTotal"  INTEGER NOT NULL DEFAULT 0,
-"dpTotal"  INTEGER NOT NULL DEFAULT 0,
-"alignedData"  INTEGER NOT NULL DEFAULT 0
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+DROP TABLE "server";
+CREATE TABLE "server" (
+"server"  TEXT(255) NOT NULL,
+"data"  TEXT(255) NOT NULL,
+"error"  TEXT(255) NOT NULL,
+"updates"  TEXT(255) NOT NULL
 );
-
--- ----------------------------
--- Records of config
--- ----------------------------
-INSERT INTO "config" VALUES (5, 15, 2, 4, 0);
-
--- ----------------------------
--- Table structure for "main"."dataPoint"
--- ----------------------------
+INSERT INTO "server" VALUES('192.168.1.16','/data','/error','/updates');
+DROP TABLE "dataSource";
+CREATE TABLE "dataSource" (
+"deviceId"  INTEGER NOT NULL,
+"deviceName"  TEXT NOT NULL,
+"modbusId"  INTEGER NOT NULL,
+"interface"  TEXT NOT NULL DEFAULT ttyS0,
+"baudRate"  INTEGER NOT NULL DEFAULT 0,
+"parity"  TEXT NOT NULL DEFAULT 0,
+"dataBits"  INTEGER NOT NULL DEFAULT 0,
+"stopBit"  INTEGER NOT NULL DEFAULT 0,
+"deviceType"  INTEGER NOT NULL DEFAULT 1,
+"deviceTimeout"  INTEGER NOT NULL DEFAULT 5,
+PRIMARY KEY ("deviceId" ASC)
+);
+INSERT INTO "dataSource" VALUES(1,'DINTEGRA1',1,'/dev/ttyS0',9600,'N',8,1,1,7);
+INSERT INTO "dataSource" VALUES(2,'ModbusTCP',1,'192.168.1.30',0,'0',0,0,2,7);
 DROP TABLE "dataPoint";
 CREATE TABLE "dataPoint" (
 "id"  INTEGER NOT NULL,
@@ -56,57 +60,24 @@ CREATE TABLE "dataPoint" (
 "regAddress"  INTEGER NOT NULL,
 "regType"  INTEGER NOT NULL,
 "modbusCode"  INTEGER NOT NULL,
+"writeValue"  INTEGER,
 "scale"  INTEGER NOT NULL DEFAULT 0,
 "scaleValue"  REAL NOT NULL DEFAULT 1,
 "offset"  REAL NOT NULL DEFAULT 0,
-"writeValue"  INTEGER,
 PRIMARY KEY ("id" ASC)
 );
-
--- ----------------------------
--- Records of dataPoint
--- ----------------------------
-INSERT INTO "dataPoint" VALUES (1, 1, 'Freq', 2, 2, 4, 0, 0, 1.0, 17.0);
-INSERT INTO "dataPoint" VALUES (2, 2, 'Write', 9, 1, 6, 991, 0, 0.0, 0.0);
-INSERT INTO "dataPoint" VALUES (3, 2, 'L2-N', 4, 8, 4, 0, 0, 10.0, 0.0);
-INSERT INTO "dataPoint" VALUES (4, 2, 'L1-N', 6, 5, 4, 0, 0, 1.0, 0.0);
-
--- ----------------------------
--- Table structure for "main"."dataSource"
--- ----------------------------
-DROP TABLE "main"."dataSource";
-CREATE TABLE "dataSource" (
-"deviceId"  INTEGER NOT NULL,
-"deviceType"  INTEGER NOT NULL DEFAULT 1,
-"deviceTimeout"  INTEGER NOT NULL DEFAULT 5,
-"deviceName"  TEXT NOT NULL,
-"modbusId"  INTEGER NOT NULL,
-"interface"  TEXT NOT NULL DEFAULT ttyS0,
-"baudRate"  INTEGER NOT NULL DEFAULT 0,
-"parity"  TEXT NOT NULL DEFAULT 0,
-"dataBits"  INTEGER NOT NULL DEFAULT 0,
-"stopBit"  INTEGER NOT NULL DEFAULT 0,
-PRIMARY KEY ("deviceId" ASC)
+INSERT INTO "dataPoint" VALUES(1,2,'Freq',2,2,4,NULL,0,1.0,0.0);
+INSERT INTO "dataPoint" VALUES(2,2,'Write',9,1,6,991,0,0.0,0.0);
+INSERT INTO "dataPoint" VALUES(3,2,'L2-N',4,8,4,NULL,0,1.0,0.0);
+INSERT INTO "dataPoint" VALUES(4,2,'L1-N',6,5,4,NULL,0,1.0,0.0);
+DROP TABLE "config";
+CREATE TABLE "config" (
+"logInterval"  INTEGER NOT NULL DEFAULT 5,
+"transferInterval"  INTEGER NOT NULL DEFAULT 30,
+"dsTotal"  INTEGER NOT NULL DEFAULT 0,
+"dpTotal"  INTEGER NOT NULL DEFAULT 0,
+"alignedData"  INTEGER NOT NULL DEFAULT 1,
+""
 );
-
--- ----------------------------
--- Records of dataSource
--- ----------------------------
-INSERT INTO "dataSource" VALUES (1, 'DINTEGRA1', 1, '/dev/ttyS1', 9600, 'N', 8, 1, 1, 7);
-INSERT INTO "dataSource" VALUES (2, 'ModbusTCP', 1, '192.168.1.30', 0, 0, 0, 0, 2, 7);
-
--- ----------------------------
--- Table structure for "main"."server"
--- ----------------------------
-DROP TABLE "main"."server";
-CREATE TABLE "server" (
-"server"  TEXT(255) NOT NULL,
-"data"  TEXT(255) NOT NULL,
-"error"  TEXT(255) NOT NULL,
-"updates"  TEXT(255) NOT NULL
-);
-
--- ----------------------------
--- Records of server
--- ----------------------------
-INSERT INTO "server" VALUES ('192.168.1.16', '/data', '/error', '/updates');
+INSERT INTO "config" VALUES(1,15,2,4,1,NULL);
+COMMIT;
